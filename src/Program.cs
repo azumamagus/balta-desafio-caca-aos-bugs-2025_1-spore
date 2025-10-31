40dec2e50463dac7062a54d76997d10e000fdbfc
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2025-10-31 14:13:15 -0300

    Ajustes

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,10 @@
+using BugStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cnnString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite(cnnString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
